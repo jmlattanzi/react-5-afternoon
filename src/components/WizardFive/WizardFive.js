0b09ba098d4f5ceff25d84a034cb252e748f47f3
Jonathan Lattanzi <jonathanlattanzi@gmail.com>
2019-01-28 15:11:33 -0600

    Created store and reducer, added actions and creators

--- src/components/WizardFive/WizardFive.js
+++ src/components/WizardFive/WizardFive.js
@@ -1,17 +1,32 @@
-import React,  { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { updateRealEstateAgent } from '../../ducks/reducer'
 
 class WizardFive extends Component {
-
-    render(){
-        return(
-            <div className="parent-div">
-                    <div className="vert-align">    
-
-                    <p>Are you currently working with a real estate agent?</p> <br />
-                    <div className="row">
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentTrue}>Yes</button></Link>
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentFalse}>No </button></Link>
+    render() {
+        return (
+            <div className='parent-div'>
+                <div className='vert-align'>
+                    <p>Are you currently working with a real estate agent?</p>{' '}
+                    <br />
+                    <div className='row'>
+                        <Link to='/wSix'>
+                            <button
+                                onClick={() =>
+                                    this.props.updateRealEstateAgent(true)
+                                }>
+                                Yes
+                            </button>
+                        </Link>
+                        <Link to='/wSix'>
+                            <button
+                                onClick={() =>
+                                    this.props.updateRealEstateAgent(false)
+                                }>
+                                No{' '}
+                            </button>
+                        </Link>
                     </div>
                 </div>
             </div>
@@ -19,4 +34,14 @@ class WizardFive extends Component {
     }
 }
 
-export default WizardFive;
+const mapStateToProps = (state) => {
+    const { realEstateAgent } = state
+    return {
+        realEstateAgent,
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    { updateRealEstateAgent }
+)(WizardFive)
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,5 +1,246 @@
-function reducer(){
-
+const initalState = {
+    loanType: 'Home Purchase',
+    propertyType: 'Single Family Home',
+    city: '',
+    propToBeUsedOn: '',
+    found: 'false',
+    realEstateAgent: 'false',
+    cost: 0,
+    downPayment: 0,
+    credit: '',
+    history: '',
+    addressOne: '',
+    addressTwo: '',
+    addressThree: '',
+    firstName: '',
+    lastName: '',
+    email: '',
 }
 
-export default reducer;
+const UPDATE_LOAN_TYPE = 'UPDATE_LOAN_TYPE'
+const UPDATE_PROPERTY_TYPE = 'UPDATE_PROPERTY_TYPE'
+const UPDATE_CITY = 'UPDATE_CITY'
+const UPDATE_PROP = 'UPDATE_PROP'
+const UPDATE_FOUND = 'UPDATE_FOUND'
+const UPDATE_REAL_ESTATE_AGENT = 'UPDATE_REAL_ESTATE_AGENT'
+const UPDATE_COST = 'UPDATE_COST'
+const UPDATE_DOWN_PAYMENT = 'UPDATE_DOWN_PAYMENT'
+const UPDATE_CREDIT = 'UPDATE_CREDIT'
+const UPDATE_HISTORY = 'UPDATE_HISTORY'
+const UPDATE_ADDRESS_ONE = 'UPDATE_ADDRESS_ONE'
+const UPDATE_ADDRESS_TWO = 'UPDATE_ADDRESS_TWO'
+const UPDATE_ADDRESS_THREE = 'UPDATE_ADDRESS_THREE'
+const UPDATE_FIRST_NAME = 'UPDATE_FIRST_NAME'
+const UPDATE_LAST_NAME = 'UPDATE_LAST_NAME'
+const UPDATE_EMAIL = 'UPDATE_EMAIL'
+
+export const updateLoanType = (loanType) => {
+    return {
+        type: UPDATE_LOAN_TYPE,
+        payload: loanType,
+    }
+}
+
+export const updatePropertyType = (property) => {
+    return {
+        type: UPDATE_PROPERTY_TYPE,
+        payload: property,
+    }
+}
+
+export const updateCity = (city) => {
+    return {
+        type: UPDATE_CITY,
+        payload: city,
+    }
+}
+
+export const updateProp = (prop) => {
+    return {
+        type: UPDATE_PROP,
+        payload: prop,
+    }
+}
+
+export const updateFound = (found) => {
+    return {
+        type: UPDATE_FOUND,
+        payload: found,
+    }
+}
+
+export const updateRealEstateAgent = (value) => {
+    return {
+        type: UPDATE_REAL_ESTATE_AGENT,
+        payload: value,
+    }
+}
+
+export const updateCost = (value) => {
+    return {
+        type: UPDATE_COST,
+        payload: value,
+    }
+}
+
+export const updateDownPayment = (value) => {
+    return {
+        type: UPDATE_DOWN_PAYMENT,
+        payload: value,
+    }
+}
+
+export const updateCredit = (value) => {
+    return {
+        type: UPDATE_CREDIT,
+        payload: value,
+    }
+}
+
+export const updateHistory = (value) => {
+    return {
+        type: UPDATE_HISTORY,
+        payload: value,
+    }
+}
+
+export const updateAddressOne = (value) => {
+    return {
+        type: UPDATE_ADDRESS_ONE,
+        payload: value,
+    }
+}
+
+export const updateAddressTwo = (value) => {
+    return {
+        type: UPDATE_ADDRESS_TWO,
+        payload: value,
+    }
+}
+
+export const updateAddressThree = (value) => {
+    return {
+        type: UPDATE_ADDRESS_THREE,
+        payload: value,
+    }
+}
+
+export const updateFirstName = (value) => {
+    return {
+        type: UPDATE_FIRST_NAME,
+        payload: value,
+    }
+}
+
+export const updateLastName = (value) => {
+    return {
+        type: UPDATE_LAST_NAME,
+        payload: value,
+    }
+}
+
+export const updateEmail = (value) => {
+    return {
+        type: UPDATE_EMAIL,
+        payload: value,
+    }
+}
+
+function reducer(state = initalState, action) {
+    switch (action.type) {
+        case UPDATE_LOAN_TYPE:
+            return {
+                ...state,
+                loanType: action.payload,
+            }
+
+        case UPDATE_PROPERTY_TYPE:
+            return {
+                ...state,
+                propertyType: action.paylaod,
+            }
+
+        case UPDATE_CITY:
+            return {
+                ...state,
+                city: action.payload,
+            }
+
+        case UPDATE_PROP:
+            return {
+                ...state,
+                props: action.payload,
+            }
+
+        case UPDATE_FOUND:
+            return {
+                ...state,
+                found: action.payload,
+            }
+
+        case UPDATE_REAL_ESTATE_AGENT:
+            return {
+                ...state,
+                agent: action.payload,
+            }
+
+        case UPDATE_COST:
+            return {
+                ...state,
+                cost: action.payload,
+            }
+
+        case UPDATE_DOWN_PAYMENT:
+            return {
+                ...state,
+                downPayment: action.payload,
+            }
+
+        case UPDATE_CREDIT:
+            return {
+                ...state,
+                credit: action.payload,
+            }
+
+        case UPDATE_HISTORY:
+            return {
+                ...state,
+                history: action.payload,
+            }
+
+        case UPDATE_ADDRESS_ONE:
+            return {
+                ...state,
+                addressOne: action.payload,
+            }
+
+        case UPDATE_ADDRESS_TWO:
+            return {
+                ...state,
+                addressTwo: action.payload,
+            }
+
+        case UPDATE_ADDRESS_THREE:
+            return {
+                ...state,
+                addressThree: action.payload,
+            }
+
+        case UPDATE_FIRST_NAME:
+            return {
+                ...state,
+                firstName: action.payload,
+            }
+
+        case UPDATE_LAST_NAME:
+            return {
+                ...state,
+                lastName: action.payload,
+            }
+
+        default:
+            return state
+    }
+}
+
+export default reducer
